d949202d25ddfc7ea9015e01bc0b7d1d0868cc6d
Nishanth <Nishanth.Suresh.Thanayil.Punathil@nn.nl>
2021-09-19 15:58:09 +0200

    render props pattern

--- src/components/Pizza.tsx
+++ src/components/Pizza.tsx
@@ -1,30 +1,38 @@
 import React from "react";
 import PizzaCSS from "./Pizza.module.css";
 import { Pizza } from "../../types";
-import { AddToCartProps, withAddToCart } from "./AddToCart";
+import { AddToCartProps, withAddToCart, WithAddToCartProps } from "./AddToCart";
 
 interface Props extends AddToCartProps {
   pizza: Pizza;
 }
 
-const PizzaItem: React.FC<Props> = ({ pizza, addToCart }) => {
-  const handleAddToCartClick = () => {
-    addToCart({
-      id: pizza.id,
-      name: pizza.name,
-      price: pizza.price,
-    });
-  };
+const PizzaItem: React.FC<Props> = ({ pizza }) => {
   return (
     <li className={PizzaCSS.container}>
       <h2>{pizza.name}</h2>
       <p>{pizza.description}</p>
       <p>{pizza.price}</p>
-      <button type="button" onClick={handleAddToCartClick}>
-        Add to Cart
-      </button>
+      <WithAddToCartProps>
+        {({ addToCart }) => {
+          return (
+            <button
+              type="button"
+              onClick={() =>
+                addToCart({
+                  id: pizza.id,
+                  name: pizza.name,
+                  price: pizza.price,
+                })
+              }
+            >
+              Add to Cart
+            </button>
+          );
+        }}
+      </WithAddToCartProps>
     </li>
   );
 };
 
-export default withAddToCart(PizzaItem);
+export default PizzaItem;
